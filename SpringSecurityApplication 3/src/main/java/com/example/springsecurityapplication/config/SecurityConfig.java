9b9a2b913717c942e589bf92f4aa997c038723dc
kreved77 <btdreport@gmail.com>
2022-12-05 00:05:45 +0300

    auth/login failure message

--- SpringSecurityApplication 3/src/main/java/com/example/springsecurityapplication/config/SecurityConfig.java
+++ SpringSecurityApplication 3/src/main/java/com/example/springsecurityapplication/config/SecurityConfig.java
@@ -71,7 +71,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .defaultSuccessUrl("/index", true)
                 // Указываем куда необходимо перенаправить пользователя при проваленной аутентификации
                 // В url будет передан объект. Данный объект мы будем проверять на форме и если он есть будет выводить сообщение "Неправильный логин или пароль"
-                .failureUrl("/auth/login")
+                .failureUrl("/auth/login?error")
                 .and()
                 // Указываем что при переходе на  /logout будет очищена сессия пользовате и перенаправление на /auth/login
                 .logout().logoutUrl("/logout").logoutSuccessUrl("/auth/login");
