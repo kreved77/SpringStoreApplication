583cfddc916fd2cafebe99ba2ff8bb67319bc09d
kreved77 <btdreport@gmail.com>
2022-12-07 23:51:02 +0300

    UPD (security settings) Login page - password change by any login

--- SpringSecurityApplication 3/src/main/java/com/example/springsecurityapplication/controllers/AuthController.java
+++ SpringSecurityApplication 3/src/main/java/com/example/springsecurityapplication/controllers/AuthController.java
@@ -51,28 +51,28 @@ public class AuthController {
         return "redirect:/index";
     }
 
-    @GetMapping("/password/change")
-    public String changePassword(Model model){
-        model.addAttribute("person", new Person());
-        return "password";
-    }
-
-    @PostMapping("/password/change")
-    public String changePassword(@ModelAttribute("person") @Valid Person person, BindingResult bindingResult){
-
-        personValidator.findUser(person, bindingResult);
-        if(bindingResult.hasErrors()){
-            return "password";
-        }
-
-        Person person_db = personService.getPersonFindByLogin(person);
-
-        int id = person_db.getId();
-        String password = person.getPassword();
-        personService.changePassword(id, password);
-
-        return "redirect:/index";
-    }
+//    @GetMapping("/password/change")
+//    public String changePassword(Model model){
+//        model.addAttribute("person", new Person());
+//        return "password";
+//    }
+//
+//    @PostMapping("/password/change")
+//    public String changePassword(@ModelAttribute("person") @Valid Person person, BindingResult bindingResult){
+//
+////        personValidator.findUser(person, bindingResult);
+////        if(bindingResult.hasErrors()){
+////            return "password";
+////        }
+//
+//        Person person_db = personService.getPersonFindByLogin(person);
+//
+//        int id = person_db.getId();
+//        String password = person.getPassword();
+//        personService.changePassword(id, password);
+//
+//        return "redirect:/index";
+//    }
 
 
     @GetMapping("/password/changepersonal")
@@ -85,10 +85,10 @@ public class AuthController {
     @PostMapping("/password/changepersonal")
     public String changePasswordPersonal(@ModelAttribute("person") @Valid Person person, BindingResult bindingResult, Model model){
         personValidator.findUser(person, bindingResult);
-        if(bindingResult.hasErrors()){
-            model.addAttribute("login", SecurityContextHolder.getContext().getAuthentication().getName());
-            return "passwordpersonal";
-        }
+//        if(bindingResult.hasErrors()){
+//            model.addAttribute("login", SecurityContextHolder.getContext().getAuthentication().getName());
+//            return "passwordpersonal";
+//        }
 
         Person person_db = personService.getPersonFindByLogin(person);
         int id = person_db.getId();
